193e04eb518e824d1dd1d9c94a420a9d5ac7338b
Pit <pittje2258585700@gmail.com>
2026-08-22 13:44:08 +0200

    entity handling and mousedown

--- javascript.js
+++ javascript.js
@@ -7,13 +7,16 @@ let rightPressed = false;
 let leftPressed = false;
 let upPressed = false;
 let downPressed = false;
+let mousePressed = false;
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
+document.addEventListener("mousedown", mouseDownHandler);
+document.addEventListener("mouseup", mouseUpHandler);
 
 const ownerID = 1
 
-const playerArray = [];
+const entityArray = [];
 
 class Vector {
   constructor(x,y) {
@@ -25,8 +28,24 @@ class Vector {
     this.y = 0
   }
 }
+/*
+class Barrel {
+  constructor(length, width) {
+    this.length = length
+    this.width = width
+    this.angle = 0
+    this.bulletstats = {
+      class: 0,
+      size: this.width,
+      speed: 0,  gonna make this rely on player stats 
 
-class Player {
+    }
+  }
+}
+  I'll be using something like this later. first i wanna see if i can get bullets to work at all
+*/
+
+class Entity {
   constructor(x, y, radius, speed, owner) {
     this.x = x;
     this.y = y;
@@ -35,6 +54,8 @@ class Player {
     this.acceleration = new Vector(0,0);
     this.speed = speed;
     this.owner = owner;
+    this.barrel = [];
+    this.isPlayer = true;
   }
 }
 
@@ -43,32 +64,32 @@ const view = {
   y: 0
 }
 
-playerArray[0] = new Player(0, 0, 30, 0.2, 1)
-playerArray[1] = new Player(50, 50, 30, 0.2, 2)
-playerArray[2] = new Player(90, -50, 20, 0.2, 2)
+entityArray[0] = new Entity(0, 0, 30, 0.2, 1)
+entityArray[1] = new Entity(50, 50, 30, 0.2, 2)
+entityArray[2] = new Entity(90, -50, 20, 0.2, 2)
 
 function setView () {
-  for (player of playerArray) {
-    if (player.owner == ownerID) {
-      view.x = player.x
-      view.y = player.y
+  for (entity of entityArray) {
+    if (entity.owner == ownerID) {
+      view.x = entity.x
+      view.y = entity.y
     }
   }
 }
 
 function drawPlayerBody () {
-  for (player of playerArray) {
-      if (player.owner == ownerID) {
-        drawOwnPlayerBody();
-    } else {
-      drawOtherPlayerBody();
-    }
+  if (entity.owner == ownerID) {
+      drawOwnPlayerBody();
+  } else {
+    drawOtherPlayerBody();
   }
+
 }
+
 
 function drawOwnPlayerBody () {
   ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height/2, player.radius, 0, Math.PI*2)
+  ctx.arc(canvas.width/2, canvas.height/2, entity.radius, 0, Math.PI*2)
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -76,14 +97,16 @@ function drawOwnPlayerBody () {
 
 function drawOtherPlayerBody () {
   ctx.beginPath();
-  ctx.arc(canvas.width/2-view.x+player.x, canvas.height/2-view.y+player.y, player.radius, 0, Math.PI*2)
+  ctx.arc(canvas.width/2-view.x+entity.x, canvas.height/2-view.y+entity.y, entity.radius, 0, Math.PI*2)
   ctx.fillStyle = "#dd0000";
   ctx.fill();
   ctx.closePath();
 }
 
-function drawPlayer () {
-  drawPlayerBody();
+function drawEntity () {
+  if (entity.isPlayer) {
+    drawPlayerBody();
+  }
 }
 
 function drawGrid () {
@@ -133,6 +156,14 @@ function keyUpHandler(e) {
   }  
 }
 
+function mouseDownHandler() {
+  mousePressed = true;
+}
+
+function mouseUpHandler() {
+  mousePressed = false;
+}
+
 function velocityCalculation (player) {
   manualMovementVelocityCalculation();
 }
@@ -141,40 +172,39 @@ function manualMovementVelocityCalculation () {
   let addedX = 0;
   let addedY = 0
   if (rightPressed) {
-    addedX += player.speed;
+    addedX += entity.speed;
   }
   if (leftPressed) {
-    addedX -= player.speed;
+    addedX -= entity.speed;
   }
   if (upPressed) {
-    addedY -= player.speed;
+    addedY -= entity.speed;
   }
   if (downPressed) {
-    addedY += player.speed;
+    addedY += entity.speed;
   }
-  if (Math.sqrt(addedX**2, addedY**2) >= player.speed) {
+  if (Math.sqrt(addedX**2, addedY**2) >= entity.speed) {
     addedX *= 2**0.5 * 0.5
     addedY *= 2**0.5 * 0.5
   }
-  player.acceleration.x += addedX;
-  player.acceleration.y += addedY;
+  entity.acceleration.x += addedX;
+  entity.acceleration.y += addedY;
 }
 
 function calculatePlayers() {
-  for (player of playerArray) {
-    if (player.owner == ownerID) {
-      velocityCalculation(player);
-    }
-    player.velocity.x += player.acceleration.x;
-    player.velocity.y += player.acceleration.y;    
-    player.x += player.velocity.x;
-    player.y +=player.velocity.y;
-    player.velocity.x = player.velocity.x * 0.9
-    player.velocity.y = player.velocity.y * 0.9
-    player.acceleration.null();
-    player.x = Math.max(Math.min(player.x, 500), -500)
-    player.y = Math.max(Math.min(player.y, 500), -500)
+
+  if (entity.isPlayer && entity.owner == ownerID) {
+    velocityCalculation();
   }
+    entity.velocity.x += entity.acceleration.x;
+    entity.velocity.y += entity.acceleration.y;    
+    entity.x += entity.velocity.x;
+    entity.y += entity.velocity.y;
+    entity.velocity.x = entity.velocity.x * 0.9
+    entity.velocity.y = entity.velocity.y * 0.9
+    entity.acceleration.null();
+    entity.x = Math.max(Math.min(entity.x, 500), -500)
+    entity.y = Math.max(Math.min(entity.y, 500), -500)
 
 
 
@@ -183,23 +213,30 @@ function calculatePlayers() {
 function drawText() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText(`x: ${playerArray[0].x}`, 8, 20);
-  ctx.fillText(`y: ${playerArray[0].y}`, 8, 40);
-  ctx.fillText(`velocity x: ${playerArray[0].velocity.x}`, 8, 60);
-  ctx.fillText(`velocity y: ${playerArray[0].velocity.y}`, 8, 80);
+  ctx.fillText(`x: ${entityArray[0].x}`, 8, 20);
+  ctx.fillText(`y: ${entityArray[0].y}`, 8, 40);
+  ctx.fillText(`velocity x: ${entityArray[0].velocity.x}`, 8, 60);
+  ctx.fillText(`velocity y: ${entityArray[0].velocity.y}`, 8, 80);
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   setView();
   drawGrid();
-  drawPlayer();
+  for (entity of entityArray) {
+    drawEntity();
+  }
   drawText();
   
 }
 
 function calculate() {
-  calculatePlayers()
+  for (entity of entityArray) {
+    calculatePlayers()
+  }
+  if (mousePressed) {
+    entityArray.push(new Entity(entityArray[0].x, entityArray[0].y, 20, 0.2, 2))
+  }
 }
 
 function gameTick () {
